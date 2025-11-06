3d35c25a4812f9881e7eb31159132e539d4d08f5
AZ technician <mra760063@gmail.com>
2025-11-06 10:43:01 +0530

    Fix home page 404 error by removing force-static exports

--- app/page.tsx
+++ app/page.tsx
@@ -15,10 +15,6 @@ export const metadata: Metadata = {
   },
 }
 
-// Force static generation
-export const dynamic = 'force-static'
-export const revalidate = false
-
 export default function Home() {
   return (
     <main>
